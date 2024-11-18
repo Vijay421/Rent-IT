9771a48f27235ca60c76a33f05d7463b9681ab80
Wix <shendrickwilliams@gmail.com>
2024-11-18 15:04:21 +0100

    Route for Login and Register pagina and Links in each one respectively

--- frontend/Rent-IT/src/components/App.jsx
+++ frontend/Rent-IT/src/components/App.jsx
@@ -1,12 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage.jsx";
+import RegisterPage from "../pages/RegisterPage.jsx";
 
 function App() {
-
-
     return (
-        <>
-            <LoginPage/>
-        </>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage/>} />
+            </Routes>
+        </Router>
     );
 }
 
