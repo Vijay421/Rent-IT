b02ecf961f50c525aadc20d96490cf07bd3ae29f
Wix <shendrickwilliams@gmail.com>
2024-11-18 12:42:53 +0100

    Updated register component, page and Register.css

--- frontend/Rent-IT/src/components/Register.jsx
+++ frontend/Rent-IT/src/components/Register.jsx
@@ -4,6 +4,8 @@ import {useState} from "react";
 export default function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState();
 
     // // Handles action when typing in email field
     function handleEmail(e) {
@@ -15,9 +17,20 @@ export default function Register() {
         setPassword(e.target.value);
     }
 
+    function handleUsername(e) {
+        setUsername(e.target.value);
+    }
+
+    function handlePhoneNumber(e) {
+        setPhoneNumber(e.target.value);
+    }
+
     return (
         <main className='register-box'>
-            <h1 className='register-box__text'>Register</h1>
+            <h1 className='register-box__text'>Registeren</h1>
+
+            <h2 className='register-box__input-text'>Naam:</h2>
+            <input className='register-box__input-field' type="text" value={username} onChange={handleUsername}/>
 
             <h2 className='register-box__input-text'>E-mail adres:</h2>
             <input className='register-box__input-field' type="text" value={email} onChange={handleEmail}/>
@@ -25,11 +38,18 @@ export default function Register() {
             <h2 className='register-box__input-text'>Wachtwoord:</h2>
             <input className='register-box__input-field' type='password' value={password} onChange={handlePassword}/>
 
-            <button className='register-box__button' type='button'>Login</button>
+            <h2 className='register-box__input-text'>Telefoonnummer:</h2>
+            <input className='register-box__input-field' type='tel' value={phoneNumber} onChange={handlePhoneNumber} maxLength='13' pattern="[0-9]*" inputMode='numeric' onKeyDown={(e) => {
+                if (!/[0-9]/.test(e.key) && e.key !== 'Backspace' && e.key !== 'Delete' && e.key !== '+' && e.key !== 'ArrowLeft' && e.key !== 'ArrowRight') {
+                    e.preventDefault();
+                }
+            }}
+            />
+
+            <button className='register-box__button' type='button'>Submit</button>
 
             <nav className="register-box__hyperlinks">
-                <a href="#">Wachtwoord vergeten? Account hier herstellen</a>
-                <a href="#">Geen account? Maak hier een account aan</a>
+                <a href="#">Heb je al een account? Klik hier om in te loggen</a>
             </nav>
         </main>
     );
