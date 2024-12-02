4b3d5fbbe2bf2e2ce8650b10189a838ac363658f
CHR1ST18N <christian.aartman@gmail.com>
2024-12-02 11:13:13 +0100

    Update Bedrijf.cs

--- backend/Models/Bedrijf.cs
+++ backend/Models/Bedrijf.cs
@@ -6,12 +6,16 @@ namespace backend.Models
     {
         public int Id { get; set; }
         [MinLength(2)]
-        public string Name { get; set; }
-        [MinLength(8)]
+        [Required]
+        public required string Name { get; set; }
+        [MinLength(8)]        
+        [Required]
         public string Address { get; set; }
         [MinLength(2)]
+        [Required]
         public int KvK_nummer { get; set; }
         [MinLength(2)]
+        [Required]
         public string? PhoneNumber { get; set; }
 
         public Bedrijf(int id, string name, string? phoneNumber)
