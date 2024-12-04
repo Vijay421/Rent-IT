3b9ebd6ac32f1423e543cbf8d312f3ee472df397
ImNeroche <mickhoogendijk@live.nl>
2024-12-04 11:12:28 +0100

    Footer fixes

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,12 +1,13 @@
 import Register from '../components/Register.jsx';
 import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function RegisterPage() {
     return (
         <>
             <Navbar/>
             <Register/>
-            <footer/>
+            <Footer/>
         </>
     );
 }
--- frontend/src/pages/RetrievePasswordPage.jsx
+++ frontend/src/pages/RetrievePasswordPage.jsx
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar.jsx";
 import RetrievePassword from "../components/RetrievePassword.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function RetrievePasswordPage() {
     return (
         <>
             <Navbar/>
             <RetrievePassword/>
-            <footer/>
+            <Footer/>
         </>
     );
 }
