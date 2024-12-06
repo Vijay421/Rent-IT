828b29bda1c041ee1b0f088c5a284a215e65d8e1
vijay_sankar <vijay99sankar@gmail.com>
2024-12-06 08:56:07 +0100

    fixed cookie not send issue

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -29,8 +29,12 @@ function Login() {
         };
 
         try {
+            debugger;
             const response = await fetch('https://localhost:53085/auth/login?useCookies=true&useSessionCookies=true', {
                 method: 'POST',
+
+                // TODO: change to 'same-origin' when in production.
+                credentials: 'include', // 'credentials' has to be defined, otherwise the auth cookie will not be send in other fetch requests.
                 headers: {
                     'content-type': 'application/problem+json'
                 },
@@ -44,7 +48,7 @@ function Login() {
                 login();
 
                 try {
-                    const userClaims = await GetUserClaims();
+                    const userClaims = await getUserClaims();
                     sessionStorage.setItem('userClaims', JSON.stringify(userClaims));
 
                     // TODO: go to profile page, instead of refreshing the page.
@@ -119,7 +123,7 @@ function Login() {
  * Will try to get the user claims of the current logged in user. 
  * @returns {Object}
  */
-async function GetUserClaims() {
+async function getUserClaims() {
     const request = {
         method: 'GET',
         credentials: 'include', // TODO: change to 'same-origin' when in production.
