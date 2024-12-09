33e5ed34a0a27b27ebf77f61f027a36dc2efd51d
Vijay <vijay99sankar@gmail.com>
2024-12-09 13:07:29 +0100

    user seeder could make all users

--- backend/Data/UserSeeder.cs
+++ backend/Data/UserSeeder.cs
@@ -7,37 +7,49 @@ namespace backend.Data
 {
     public class UserSeeder
     {
-          public async Task Seed(UserManager<User> userManager, IConfiguration config)
-          {
-            await SeedAdmin(userManager, config);
-          }
-
-        private async Task SeedAdmin(UserManager<User> userManager, IConfiguration config)
+        public async Task Seed(UserManager<User> userManager, IConfiguration config)
         {
-            var adminConfig = config.GetSection("accounts").GetSection("admin");
-            if (!adminConfig.Exists())
+            await Seed(userManager, config, configName: "admin", role: "admin");
+            await Seed(userManager, config, configName: "beheerder", role: "zakelijke_beheerder");
+        }
+
+        /// <summary>
+        /// Creates a user if the name is not taken already.
+        /// </summary>
+        /// <param name="userManager"></param>
+        /// <param name="config"></param>
+        /// <param name="configName">Corresponds to the name of the key in the 'accounts' object in local_config.json.</param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private async Task Seed(UserManager<User> userManager, IConfiguration config, string configName, string role)
+        {
+            var userConfig = config.GetSection("accounts").GetSection(configName);
+            if (!userConfig.Exists())
             {
                 return;
             }
 
-            // Create the admin only when it does not exists already.
-            var admin = await userManager.FindByNameAsync("admin");
-            if (admin != null)
+            var userName = userConfig["userName"] ?? throw new Exception($"no userName field for '{role}' in local_config.json");
+
+            // Create the user only when it does not exists already.
+            var user = await userManager.FindByNameAsync(userName);
+            if (user != null)
             {
                 return;
             }
 
-            admin = new User
+            user = new User
             {
-                UserName = adminConfig["userName"] ?? throw new Exception("no userName field for admin in local_config.json"),
-                Email = adminConfig["email"] ?? throw new Exception("no email field for admin in local_config.json"),
+                UserName = userName,
+                Email = userConfig["email"] ?? throw new Exception($"no email field for '{role}' in local_config.json"),
                 EmailConfirmed = true,
             };
-            var result = await userManager.CreateAsync(admin, adminConfig["password"] ?? throw new Exception("no password field for admin in local_config.json"));
+            var result = await userManager.CreateAsync(user, userConfig["password"] ?? throw new Exception("no password field for admin in local_config.json"));
 
             if (result.Succeeded)
             {
-                await userManager.AddToRoleAsync(admin, "admin");
+                await userManager.AddToRoleAsync(user, role);
             }
             else
             {
