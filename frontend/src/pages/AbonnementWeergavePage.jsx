71aea32d3ca9a3f53ee2ce936c46400738e52ba4
CHR1ST18N <christian.aartman@gmail.com>
2024-12-11 13:13:25 +0100

    Create AbonnementWeergavePage.jsx

---
+++ frontend/src/pages/AbonnementWeergavePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar.jsx";
+import Login from "../components/AbonnementWeergave.jsx";
+import Footer from "../components/Footer.jsx";
+
+export default function AbonnementWeergavePage() {
+    return (
+        <>
+            <Navbar/>
+            <AbonnementWeergave/>
+            <Footer/>
+        </>
+    );
+}
