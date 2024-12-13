fcca8bd96f4dbd7dd4f395ebb970261e53647d96
ImNeroche <mickhoogendijk@live.nl>
2024-12-13 12:25:34 +0100

    SubscriptionRequest Page fixes

--- frontend/src/components/SubscriptionRequest.jsx
+++ frontend/src/components/SubscriptionRequest.jsx
@@ -7,7 +7,7 @@ function SubscriptionRequest() {
     const [adress, setAdress] = useState("");
     const [kvkNumber, setKvkNumber] = useState("");
     const [subscriptionType, setSubscriptionType] = useState("");
-    const [confirmationMessage, setConfirmationMessage] = useState(""); // New state for message
+    const [confirmationMessage, setConfirmationMessage] = useState("");
 
     function handleBedrijfsnaam(e) {
         setBedrijfsnaam(e.target.value);
@@ -17,77 +17,88 @@ function SubscriptionRequest() {
         setAdress(e.target.value);
     }
 
-    function handleKvkNumber(e) {
-        setKvkNumber(e.target.value);
-    }
-
     function handleSubscriptionType(e) {
         setSubscriptionType(e.target.value);
     }
 
     function handleSubmit() {
-        if (bedrijfsnaam && adress && kvkNumber && subscriptionType) {
-            setConfirmationMessage("Uw abonnement aanvraag is verzonden!");
+        if (
+            bedrijfsnaam &&
+            adress &&
+            kvkNumber &&
+            subscriptionType
+        ) {
+            setConfirmationMessage("Uw abonnementhouders aanvraag is verzonden!");
         } else {
             setConfirmationMessage("Vul alstublieft alle velden in.");
         }
     }
+
 
     return (
         <main className={styles.MainDiv}>
             <p className="MainDiv__Text">Vul hieronder de gegevens in om een abonnement aan te vragen.</p>
 
             <div className="form-group">
-                <label htmlFor="abonnement__bedrijfsnaam" className='form__text'>Bedrijfsnaam:</label>
+                <label htmlFor="abonnement__bedrijfsnaam" className="form__text">Bedrijfsnaam:</label>
                 <input
                     id="abonnement__bedrijfsnaam"
-                    className='bedrijfsnaam__text'
+                    className="bedrijfsnaam__text"
                     type="text"
                     placeholder="Vul hier uw bedrijfsnaam in voor het aanvragen van een abonnement."
                     value={bedrijfsnaam}
-                    minLength='2'
-                    maxLength='50'
+                    minLength="2"
+                    maxLength="50"
                     onChange={handleBedrijfsnaam}
                 />
-                <label htmlFor="adress" className='form__text'>Adres:</label>
+                <label htmlFor="adress" className="form__text">Adres:</label>
                 <input
                     id="abonnement__adress"
-                    className='adress__text'
+                    className="adress__text"
                     type="text"
                     placeholder="Vul hier uw adres in voor het aanvragen van een abonnement."
                     value={adress}
-                    minLength='2'
-                    maxLength='50'
+                    minLength="2"
+                    maxLength="50"
                     onChange={handleAdress}
                 />
-                <label htmlFor="kvk-number" className='form__text'>KVK-nummer:</label>
+                <label htmlFor="kvk-number" className="form__text">KVK-nummer:</label>
                 <input
                     id="abonnement__kvk-number"
-                    className='kvk-number__text'
-                    type="text"
+                    className="kvk-number__text"
+                    type="number"
                     placeholder="Vul hier uw KVK-nummer in voor het aanvragen van een abonnement."
                     value={kvkNumber}
-                    minLength='2'
-                    maxLength='50'
-                    onChange={handleKvkNumber}
+                    minLength="2"
+                    maxLength="50"
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        if (/^\d*$/.test(value)) {
+                            setKvkNumber(value);
+                        }
+                    }}
                 />
-                <label htmlFor="subscription-type" className='form__text'>Type Abonnement:</label>
-                <div onChange={handleSubscriptionType}>
+                <label htmlFor="subscription-type" className="form__text">Type Abonnement:</label>
+                <div className="radio-group" onChange={handleSubscriptionType}>
                     <label>
                         <input type="radio" id="PayAsYouGo" name="subscriptionType" value="PayAsYouGo" /> Pay as you go
-                    </label><br/>
+                    </label>
                     <label>
                         <input type="radio" id="Prepaid" name="subscriptionType" value="prepaid" /> Prepaid
-                    </label><br/>
+                    </label>
                 </div>
             </div>
-
-            {/* Display confirmation message */}
             {confirmationMessage && (
-                <p className="confirmation-message">{confirmationMessage}</p>
+                <p
+                    className={`confirmation-message ${
+                        confirmationMessage === "Uw abonnementhouders aanvraag is verzonden!"
+                            ? "success"
+                            : "error"
+                    }`}
+                >
+                    {confirmationMessage}
+                </p>
             )}
-
-            {/* Button triggers handleSubmit */}
             <div onClick={handleSubmit} className="submit-button">
                 <div className="submit-button__div">
                     <p className="submit-button__text">Verstuur abonnement aanvraag</p>
