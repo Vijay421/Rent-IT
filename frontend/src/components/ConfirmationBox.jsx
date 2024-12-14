fed296c30fafaa3110b20635d165c6f7ca46445c
Wix <shendrickwilliams@gmail.com>
2024-12-14 21:29:13 +0100

    amount of days & price calculations logic, added korting field for later use and minor CSS refactoring

--- frontend/src/components/ConfirmationBox.jsx
+++ frontend/src/components/ConfirmationBox.jsx
@@ -6,22 +6,25 @@ export default function ConfirmationBox() {
     const navigate = useNavigate();
     const { vehicleData, userData, startDatum, eindDatum } = location.state;
 
-    const basePrice = vehicleData.prijs * 4;
-    const insurance = vehicleData.soort === "Auto" ? 25.50 * 4 : vehicleData.soort === "Caravan" ? 20 * 4 : 30 * 4;
-    const tax = (basePrice) * 0.21;
-    const fuel = vehicleData.soort === "Auto" ? 30 : vehicleData.soort === "Caravan" ? 0 : 50;
-    const kmCharge = 0.30 * userData.verwachtekm;
+    const amountOfDays = Math.floor((new Date(eindDatum) - new Date(startDatum)) / (1000 * 60 * 60 * 24));
+
+    const carPrice = vehicleData.prijs * amountOfDays;
+    const insurance = vehicleData.soort === "Auto" ? 15.50 * amountOfDays : vehicleData.soort === "Caravan" ? 22 * amountOfDays : 0;
+    const fuel = vehicleData.soort === "Auto" ? 50 : vehicleData.soort === "Caravan" ? 0 : 100;
+    const kmCharge = 0.23 * userData.verwachtekm;
     const deposit = vehicleData.soort === "Auto" ? 400 : vehicleData.soort === "Caravan" ? 750 : 1500;
-    const totalCost = basePrice + insurance + tax + fuel + kmCharge + deposit;
+    const tax = (carPrice + insurance + kmCharge) * 0.21;
+    const korting = 0; /*utilize later after adding korting column*/
+    const totalCost = carPrice + insurance + fuel + kmCharge + deposit + tax - korting;
 
 
     function handleAkkoordButtonClick() {
         const statusText = document.getElementById('confirmation-button-box-status__span');
 
         const payload = {
-            id: 0,
-            particuliereHuurderId: 1,
-            voertuigId: vehicleData.id,
+            id: 0, /*huuraanvraag id*/
+            particuliereHuurderId: 1, /*has to be modified later so store the current users account Id*/
+            voertuigId: vehicleData.id, /*selected vehicles id*/
             startdatum: startDatum,
             einddatum: eindDatum,
             wettelijke_naam: userData.naam,
@@ -91,17 +94,20 @@ export default function ConfirmationBox() {
                             <p>Rijbewijsnummer:</p>
                             <p>{userData.rbwnr}</p>
 
+                            <p>Reisaard:</p>
+                            <p>{userData.reisaard}</p>
+
                             <p>Start punt:</p>
                             <p>{userData.startPunt}</p>
 
                             <p>Eind punt:</p>
                             <p>{userData.eindPunt}</p>
 
-                            <p>Reisaard:</p>
-                            <p>{userData.reisaard}</p>
-
                             <p>Verste bestemming:</p>
                             <p>{userData.verstePunt}</p>
+
+                            <p>Aantal dagen:</p>
+                            <p>{amountOfDays} dagen</p>
 
                             <p>Verwachte gereden km:</p>
                             <p>{userData.verwachtekm} km</p>
@@ -135,37 +141,50 @@ export default function ConfirmationBox() {
 
                             <div className='voertuig-box-column2__div'>
                                 <p>Huurprijs:</p>
-                                <p>€ {vehicleData.prijs}</p>
+                                <p>€ {amountOfDays * vehicleData.prijs} ({amountOfDays} x € {vehicleData.prijs})</p>
 
                                 <p>Verzekering:</p>
-                                {vehicleData.soort === "Auto" && <p>€ 25,50 / dag</p>}
-                                {vehicleData.soort === "Caravan" && <p>€ 20 / dag</p>}
-                                {vehicleData.soort === "Camper" && <p>€ 30 / dag</p>}
+                                {vehicleData.soort === "Auto" &&
+                                    <p>€ {amountOfDays * 15.50} ({amountOfDays} x € 15.50)</p>}
+                                {vehicleData.soort === "Caravan" &&
+                                    <p>€ {amountOfDays * 22} ({amountOfDays} x € 22)</p>}
+                                {vehicleData.soort === "Camper" && <p>€ 0 (Eigen verzekering)</p>}
 
                                 <p>Belasting:</p>
-                                <p>€ {((vehicleData.prijs * 4) * 0.21).toFixed(2)}</p>
-
-                                <p>Benzine:</p>
-                                {vehicleData.soort === "Auto" && <p>€ 30</p>}
-                                {vehicleData.soort === "Caravan" && <p>€ 0</p>}
-                                {vehicleData.soort === "Camper" && <p>€ 50</p>}
+                                <p>€ {tax.toFixed(2)} (21%)</p>
 
                                 <p>Km vergoeding:</p>
-                                <p>€ 0.30 / km</p>
+                                <p>€{(0.23 * userData.verwachtekm).toFixed(2)} (0.23 / {userData.verwachtekm} km)</p>
 
                                 <p>Borg:</p>
-                                {vehicleData.soort === "Auto" && <p>€ 400</p>}
-                                {vehicleData.soort === "Caravan" && <p>€ 750</p>}
-                                {vehicleData.soort === "Camper" && <p>€ 1500</p>}
+                                {vehicleData.soort === "Auto" && <p>€ 400*</p>}
+                                {vehicleData.soort === "Caravan" && <p>€ 750*</p>}
+                                {vehicleData.soort === "Camper" && <p>€ 1500*</p>}
+
+                                <p>Benzine:</p>
+                                {vehicleData.soort === "Auto" && <p>€ 50**</p>}
+                                {vehicleData.soort === "Caravan" && <p>€ 0</p>}
+                                {vehicleData.soort === "Camper" && <p>€ 100**</p>}
+
+                                <p>Korting:</p>
+                                <p>€ 0</p>
                             </div>
 
                             <div className="voertuig-box-column3__div">
-                                <p className='voertuig-box-column3-title1-paragraph__p'>Huurperiode</p>
-                                <p className='voertuig-box-column3-title2-paragraph__p'>Totale kosten</p>
+                                <p>* De borgsom wordt terugbetaald als het voertuig in dezelfde staat wordt
+                                    teruggebracht als waarin het is ontvangen.</p>
+                                <p>** De brandstofkosten worden terugbetaald als het voertuig met een volle tank wordt
+                                    ingeleverd.</p>
                             </div>
+
                             <div className="voertuig-box-column4__div">
-                                <p className='voertuig-box-column3-answer1-paragraph__p'>{startDatum} – {eindDatum}</p>
-                                <p className='voertuig-box-column3-answer2-paragraph__p'>
+                                <p className='voertuig-box-column4-title1-paragraph__p'>Huurperiode</p>
+                                <p className='voertuig-box-column4-title2-paragraph__p'>Totale kosten</p>
+                            </div>
+
+                            <div className="voertuig-box-column5__div">
+                                <p className='voertuig-box-column5-answer1-paragraph__p'>{startDatum} – {eindDatum}</p>
+                                <p className='voertuig-box-column5-answer2-paragraph__p'>
                                     € {totalCost.toFixed(2)}
                                 </p>
                             </div>
@@ -175,7 +194,9 @@ export default function ConfirmationBox() {
             </div>
 
             <div className="confirmation-page-button-box__div">
-                <h3 className='confirmation-button-box__h3'>Ik (huurder) bevestig hierbij dat alle hierboven weergegeven gegevens correct zijn en ga akkoord met het verzenden van dit verzoek en het openen van de betaalpagina.</h3>
+                <h3 className='confirmation-button-box__h3'>Ik (huurder) bevestig hierbij dat alle hierboven weergegeven
+                    gegevens correct zijn en ga akkoord met het verzenden van dit verzoek en het openen van de
+                    betaalpagina.</h3>
                 <button onClick={handleAkkoordButtonClick} className='confirmation-button-box__button'>Akkoord</button>
                 <span id='confirmation-button-box-status__span'></span>
             </div>
