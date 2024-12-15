849ad8f20b5e1654cef8f0e97900ebcddf249376
ImNeroche <mickhoogendijk@live.nl>
2024-12-15 15:40:20 +0100

    temporary profile button in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,23 @@
 import Logo from '../assets/Logo.png';
 import NavButton from './NavButton.jsx';
 import '../styles/Navbar.css';
-import {Link} from "react-router-dom";
-import {AuthContext} from "./AuthContext.jsx";
-import {useContext} from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "./AuthContext.jsx";
+import { useContext} from "react";
 
 function Navbar() {
-    const {isLoggedIn, logout} = useContext(AuthContext);
 
-    return(
+    const { isLoggedIn, logout } = useContext(AuthContext);
+
+    return (
         <nav className='navbar'>
-            <Link to="/"><img className='navbar-logo__img' src={Logo} alt="CarAndAll logo"/></Link>
+            <Link to="/">
+                <img className='navbar-logo__img' src={Logo} alt="CarAndAll logo" />
+            </Link>
 
             <div className='navbar__navigation-buttons'>
-                <NavButton className="navbar_navigation-buttons-nagivation" title="Home" link="/"></NavButton>
-                <NavButton className="navbar_navigation-buttons-nagivation" title="Huren" link="/renting"></NavButton>
+                <NavButton className="navbar_navigation-buttons-nagivation" title="Home" link="/" />
+                <NavButton className="navbar_navigation-buttons-nagivation" title="Huren" link="/renting" />
             </div>
 
             {isLoggedIn ? (
@@ -25,6 +28,10 @@ function Navbar() {
                 </ul>
             ) : (
                 <ul className='navbar__login-box__button'>
+                    <Link to="/profile">
+                        <button>Profiel</button>
+                    </Link>
+
                     <Link to="/login">
                         <button>Login</button>
                     </Link>
