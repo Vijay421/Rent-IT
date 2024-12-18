91914850f6b21f24c064e0df75e9e2614599d10f
vijay_sankar <vijay99sankar@gmail.com>
2024-12-18 14:42:53 +0100

    added unit tests on the backend, with a db and user mock

---
+++ BackendTestProject/MockUtils.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendTestProject
+{
+    public class MockUtils
+    {
+        public static ClaimsPrincipal CreateMockUser(string role)
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Role, role),
+                },
+                "mock"
+            ));
+        }
+
+        public static ControllerContext CreateMockControllerContext(ClaimsPrincipal user)
+        {
+            return new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+        }
+    }
+}
