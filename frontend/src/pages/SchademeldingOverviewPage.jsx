6c2d271dd719d51692d3ef94472e2e1fde52dbfe
CHR1ST18N <christian.aartman@gmail.com>
2025-01-09 13:42:52 +0100

    added schademeldingoverview pagina voor backoffice

---
+++ frontend/src/pages/SchademeldingOverviewPage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar.jsx";
+import SchademeldingOverview from "../components/SchademeldingOverview.jsx";
+import Footer from "../components/Footer.jsx";
+
+export default function RentingSubmitPage() {
+    return (
+        <>
+            <Navbar/>
+            <SchademeldingOverview/>
+            <Footer/>
+        </>
+    );
+}
