c7dc91519672ddb54a046cfb629d0d5b4bbe8416
Wix <shendrickwilliams@gmail.com>
2025-01-17 13:19:49 +0100

    index pagina fix

--- frontend/src/components/IndexPageButton.jsx
+++ frontend/src/components/IndexPageButton.jsx
@@ -1,31 +1,48 @@
 import '../styles/IndexPageButton.css';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {AuthContext} from "./AuthContext.jsx";
 
 function IndexPageButton() {
+    const { isLoggedIn } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
     return (
-        <div className="Content">
-            <div className="MainDivLeft">
-                <div className="ParticulierDiv">
-                    <h1 className="ParticulierH1">Particulier</h1>
-                </div>
-                <Link to='/huur-overzicht'>
-                    <div className="ButtonDivLeft">
-                        <h1 className="BottomDivLeftHuren">Huren</h1>
+        <>
+            <h1 className='index-page-title__h1'>CarAndAll</h1>
+            <h2 className='index-page-title-desc__h2'>Huur zonder gedoe een voertuig, wanneer u maar wilt.</h2>
+            <div className="Content">
+                <div className="MainDivLeft">
+                    <div className="ParticulierDiv">
+                        <h3 className="ParticulierH1">Voertuig huren</h3>
                     </div>
-                </Link>
-            </div>
-            <div className="MainDivRight">
-                <div className="ZakelijkDiv">
-                    <h1 className="ZakelijkH1">Zakelijk</h1>
+                    <div className='ParticulierDiv2'>
+                        <h4>Wilt u een van onze voertuigen huren? Klik op de onderstaande knop om het huurproces te starten!</h4>
+                    </div>
+                    <div className="ParticulierDiv3">
+                        <button onClick={() => navigate("/huur-overzicht", {})} className="ButtonDivLeft">
+                            <h5 className="BottomDivLeftHuren">Huren</h5>
+                        </button>
+                    </div>
                 </div>
 
-                <Link to='/huur-overzicht'>
-                    <div className="ButtonDivRight">
-                        <h1 className="BottomDivRightHuren">Huren</h1>
+
+                <div className="MainDivRight">
+                    <div className="ZakelijkDiv">
+                        <h3 className="ZakelijkH1">Zakelijke gebruiker</h3>
                     </div>
-                </Link>
+                    <div className='ZakelijkDiv2'>
+                        <h4>Bent u een bedrijf dat voertuigen wil verhuren voor hun werknemers? Klik op de onderstaande knop om het aanvraagproces voor een zakelijk account te starten!</h4>
+                    </div>
+                    <div className="ZakelijkDiv3">
+                        <button onClick={() => navigate("/*", {})} className="ButtonDivRight">
+                            <h5 className="BottomDivRightHuren">Zakelijk account aanvragen</h5>
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
