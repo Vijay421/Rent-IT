7a3aa093d68a80b152d72eaaf063eff119949e68
Wix <shendrickwilliams@gmail.com>
2025-01-20 10:26:29 +0100

    /profiel pagina scaling fix

--- frontend/src/components/Profile/ProfilePageParticulier.jsx
+++ frontend/src/components/Profile/ProfilePageParticulier.jsx
@@ -5,7 +5,7 @@ function ProfilePageParticulier() {
 
     return (
         <ProfilePageBase>
-            <ProfilePageLinkButton link="/account-instellingen" text="Account instellingen aanpassen"/>
+            <ProfilePageLinkButton link="/account-instellingen" text="Instelligen aanpassen"/>
             <ProfilePageLinkButton link="/abonnementen-overzicht" text="Neem zakelijk abonnement"/>
             <ProfilePageLinkButton link="/huur-overzicht" text="Voertuig huren"/>
             <ProfilePageLinkButton link="/reserveringen" text="Reserveringen"/>
