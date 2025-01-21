ea050fa937e7b1f081b5090d27618db7502315e1
Wix <shendrickwilliams@gmail.com>
2025-01-21 14:25:21 +0100

    commit

--- frontend/src/components/EmployeeBox.jsx
+++ frontend/src/components/EmployeeBox.jsx
@@ -1,41 +1,39 @@
 import PropTypes from "prop-types";
 import '../styles/EmployeeBox.css';
 
-export default function EmployeeBox() {
-
-
+export default function EmployeeBox({data}) {
     return (
         <main className='employee-box__main'>
-            <h2 className='employee-box-title__h2'>employee name</h2>
+            <h2 className='employee-box-title__h2'>{data.wettelijke_naam}</h2>
             <div className="employee-box-details__div">
                 <div className="employee-box-voertuig-details__div">
                     <div className="employee-box-voertuig__div">
                         <p className="employee-box-voertuig-title__p">Voertuig</p>
-                        <p className="employee-box-voertuig-data__p">toyota corolla</p>
+                        <p className="employee-box-voertuig-data__p">{data.voertuig.merk}</p>
                     </div>
 
                     <div className="employee-box-kenteken__div">
                         <p className="employee-box-voertuig-title__p">Kenteken</p>
-                        <p className="employee-box-voertuig-data__p">AB-123-CD</p>
+                        <p className="employee-box-voertuig-data__p">{data.voertuig.kenteken}</p>
                     </div>
 
                     <div className="employee-box-soort__div">
                         <p className="employee-box-voertuig-title__p">Soort</p>
-                        <p className="employee-box-voertuig-data__p">Auto</p>
+                        <p className="employee-box-voertuig-data__p">{data.voertuig.soort}</p>
                     </div>
                 </div>
 
                 <div className="employee-box-huurperiode-details__div">
                     <div className="employee-box-huurperiode__div">
                         <p className="employee-box-huurperiode-title__p">Huurperiode</p>
-                        <p className="employee-box-huurperiode-data__p">01-01-2025 - 03-01-2025</p>
+                        <p className="employee-box-huurperiode-data__p">{data.startdatum} - {data.einddatum}</p>
                     </div>
                 </div>
 
                 <div className="employee-box-kosten-details__div">
                     <p className="employee-box-kosten-title__p">Kosten</p>
                     <div className="employee-box-totaal__div">
-                        <p className="employee-box-kosten-data__p">€ 51 p/dag</p>
+                        <p className="employee-box-kosten-data__p">€ {data.voertuig.prijs} p/dag</p>
                         <p className="employee-box-kosten-data__p">€ 5100.00*</p>
                     </div>
                 </div>
