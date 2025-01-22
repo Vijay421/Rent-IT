7fafda8b905b5571015a6f32024442a814b52186
Wix <shendrickwilliams@gmail.com>
2025-01-22 10:21:42 +0100

    duplicate fix

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -69,32 +69,7 @@ function App() {
                 <Route path="/voertuig-staten" element=         {<VoertuigStatenPage/>}/>
                 <Route path="/privacy" element=                 {<PrivacyPage/>}/>
                 <Route path="/abonnementsoverzicht" element=    {<AbonnementOverzichtPage/>}/>
-                <Route path="/" element={<IndexPage />} />
-                <Route path="/registreren" element={<RegisterPage/>} />
-                <Route path="/medewerker-aanmaken" element={<RegisterPage/>} />
-                <Route path="/medewerkersoverzicht" element={<EmployeeOverviewPage/>} />
-                <Route path="/login" element={<LoginPage/>} />
-                <Route path="/wachtwoord-vergeten" element={<RetrievePasswordPage/>} />
-                <Route path="/huur-overzicht" element={<RentingPage/>} />
-
-                <Route path="/voertuigoverzicht" element={<VehicleOverviewPage/>} />
-                <Route path="/voertuig-aanpassen" element={<VehicleCreateAndUpdate/>} />
-
-                <Route path="/profiel" element={<ProfilePage/>}/>
-                <Route path="/account-instellingen" element={<AccountSettings/>}/>
-
-                <Route path="/abonnement" element={<SubscriptionRequestPage/>}/>
-                <Route path="/abonnementen" element={<SubscriptionsManagePage/>}/>
-
-                <Route path="/huur-indienen" element={<RentingSubmitPage/>}/>
-                <Route path="/huur-geschiedenis" element={<RentHistory/>}/>
-                <Route path="/huuraanvraag-beoordelen" element={<ReviewRentRequest/>}/>
-                <Route path="/notificaties" element={<NotificationsPage/>}/>
-                <Route path="/bevestiging" element={<ConfirmationPage/>}/>
-                <Route path="/huur-betaling" element={<RentingPaymentPage/>}/>
-                <Route path="/reserveringen" element={<ReserveringPage/>}/>
-                <Route path="/reservering-wijziging" element={<ReserveringWijzigingPage/>}/>
-                <Route path="/zb-huurgeschiedenis" element={<ZbRentHistoryPage/>}/>
+                <Route path="/zb-huurgeschiedenis" element=     {<ZbRentHistoryPage/>}/>
             </Routes>
         </Router>
     );
